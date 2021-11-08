7ff56dab3c69c1fd63ad37a4d6f7dbb92c53d5e5
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-08 23:11:27 +0200

    fix app.use proxy

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ const express = require("express");
 const url = require('url');
 const proxy = require('express-http-proxy');
 
-// New hostname+path as specified by question:
-const apiProxy = proxy('https://tamweelaqar.herokuapp.com', {
-    proxyReqPathResolver: req => url.parse(req.baseUrl).path
-});
 const app = express();
 app.use(express.static(__dirname + '/dist/tamweelaqar'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, '/dist/tamweelaqar', 'index.html'))
+res.sendFile(path.join(__dirname, '/dist/tamweelaqar', 'index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.use('/api/*', apiProxy);
+app.use('/api', proxy({target: 'https://tamweelaqar.herokuapp.com', changeOrigin: true}));
