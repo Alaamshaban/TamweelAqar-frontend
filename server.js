2b4fc1c1c3205a8d16c94173f505ce3a671402b8
Alaa SHABAAN <alaa.shabaan@valeo.com>
2021-11-08 22:08:50 +0200

    install express-proxy

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const path = require("path");
 const express = require("express");
+const url = require('url');
+const proxy = require('express-http-proxy');
+
+// New hostname+path as specified by question:
+const apiProxy = proxy('https://tamweelaqar.herokuapp.com', {
+    proxyReqPathResolver: req => url.parse(req.baseUrl).path
+});
 const app = express();
 app.use(express.static(__dirname + '/dist/tamweelaqar'));
 app.get('/*', function(req,res){
-    request('https://tamweelaqar.herokuapp.com' , function (error, response, body) { 
-        if (!error && response.statusCode === 200) { 
-          console.log('body',body); 
-          res.sendFile(path.join(__dirname, '/dist/tamweelaqar', 'index.html'))
-        } 
-       }); 
+res.sendFile(path.join(__dirname, '/dist/tamweelaqar', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-
-app.get('/api', function(req, res){ 
-    request('https://api.brewerydb.com/v2/?key=' + API_KEY, function (error, response, body) { 
-      if (!error && response.statusCode === 200) { 
-        console.log(body); 
-        res.send(body); 
-      } 
-     }); 
-  });
+app.use('/api/*', apiProxy);
